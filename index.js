d9d48c58481ecbe001b53b3d703aa0966b7af3e5
ishwar-ikm <109938051+ishwar-ikm@users.noreply.github.com>
2022-08-05 15:33:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function () {
+function main () {
   var rand = Math.floor(Math.random()*6) + 1;
   var diceimg = "images/dice" + rand + ".png";
   document.querySelectorAll("img")[0].setAttribute("src", diceimg);
